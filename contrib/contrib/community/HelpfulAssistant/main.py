d6caf4552135c00b3a1cd12cd01c18c3bdef077e
hardikmaheshwari <hardik121998@gmail.com>
2024-05-22 10:44:40 +0530

    Contribute agent: Helpful assistant

---
+++ contrib/contrib/community/HelpfulAssistant/main.py
@@ -0,0 +1,8 @@
+
+from taskgen import Agent
+
+# Author: hardikmaheshwari
+class HelpfulAssistant(Agent):
+    def __init__(self):
+        super().__init__(agent_name="Helpful assistant",
+                         agent_description="You are a generalist agent")
