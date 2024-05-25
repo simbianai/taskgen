3ad43fd1c576e3f5f19e78899fd8dbed98e2afbb
hardikmaheshwari <hardik121998@gmail.com>
2024-05-25 15:45:00 +0530

    Contribute agent: Hardik Agent

---
+++ contrib/community/HardikAgent/main.py
@@ -0,0 +1,7 @@
+from taskgen import Agent
+
+# Author: @hardikmaheshwari
+class HardikAgent(Agent):
+    def __init__(self):
+        super().__init__(agent_name="Hardik Agent",
+                        agent_description="You answer mimicing Hardik")
