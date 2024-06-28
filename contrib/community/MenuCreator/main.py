d72b91796f3e7c2e79708f61bbeb05ace14bc37f
Hardik Maheshwari <19693874+hardikmaheshwari@users.noreply.github.com>
2024-06-28 14:54:06 +0530

    Contribute agent: Menu Creator

---
+++ contrib/community/MenuCreator/main.py
@@ -0,0 +1,42 @@
+from taskgen import Agent, Function, Memory, Ranker
+import math
+from Chef import Chef
+from Boss import Boss
+from CreativeWriter import CreativeWriter
+from Economist import Economist
+
+
+# Author: @hardikmaheshwari
+class MenuCreator(Agent):
+    def __init__(self):
+
+        var_agent_Chef = Chef()
+        var_agent_Boss = Boss()
+        var_agent_CreativeWriter = CreativeWriter()
+        var_agent_Economist = Economist()
+
+        super().__init__(
+            agent_name="Menu Creator",
+            agent_description='''Creates a menu for a restaurant. Menu item includes Name, Description, Ingredients, Pricing.''',
+            max_subtasks=5,
+            summarise_subtasks_count=5,
+            memory_bank={'Function': Memory(memory=[], top_k=5, mapper=lambda x: x.fn_name + ': ' + x.fn_description, approach='retrieve_by_ranker', ranker=Ranker(model='text-embedding-3-small', ranking_fn=None)),},
+            shared_variables={},
+            get_global_context=None,
+            global_context='''''',
+            default_to_llm=True,
+            code_action=False,
+            verbose=True,
+            debug=False
+        )
+
+        self.assign_functions(
+            []
+        )
+
+        self.assign_agents(
+            [var_agent_Chef,var_agent_Boss,var_agent_CreativeWriter,var_agent_Economist]
+        )
+                        
+# Supporting Functions
+
