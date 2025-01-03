9c608a3ec10b56ac38739a3bdb55b30789ee916e
vishwas <vishwas@simbian.ai>
2025-01-03 16:40:25 +0530

    build action

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+python3.11 -m venv venv
+source venv/bin/activate
+pip install wheel
+pip install toml-cli
+
+VERSION=$(toml get --toml-path pyproject.toml project.version)
+
+WHL_FILE=dist/taskgen_ai-${VERSION}-py3-none-any.whl
+rm -f ${WHL_FILE}
+
+python setup.py bdist_wheel
+
+if [ ! -f ${WHL_FILE} ]; then
+   echo "ERROR: ${WHL_FILE} was not generated"
+   exit -1
+fi
+
+echo "-----------------------------------------------"
+echo "File ${WHL_FILE} created"
