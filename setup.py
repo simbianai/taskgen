dca89d53d104e62cdb17e751b8d42cf165498835
Vishwas Pai <vishwas@simbian.ai>
2025-07-16 12:58:03 +0530

    Updating dep requirements and bump taskgen to 3.3.10 (#69)

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="taskgen",
-    version="3.3.9",
+    version="3.3.10",
     packages=find_packages(),
     install_requires=[
-        "openai>=1.3.6",
+        "openai>=1.59.6",
         "langchain",
-        "dill>=0.3.7",
-        "termcolor>=2.3.0",
+        "dill>=0.3.9",
+        "termcolor>=3.1.0",
         "requests",
-        "pypdf~=5.7.0",
+        "pypdf~=5.8.0",
         "python-docx",
         "pandas",
         "chromadb",
         "xlrd",
-        "chromadb>=0.5.2",
+        "chromadb>=1.0.15",
         "asyncio",
     ],
 )
