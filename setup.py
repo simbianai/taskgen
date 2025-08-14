344a6c135829287673f9b26e5497fb7463ab9eb1
Vishwas Pai <vishwas@simbian.ai>
2025-08-14 12:45:53 +0530

    Updating pypdf to 6.0.0 for CVE-2025-55197 (#70)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="taskgen",
-    version="3.3.10",
+    version="3.3.11",
     packages=find_packages(),
     install_requires=[
         "openai>=1.59.6",
@@ -10,7 +10,7 @@ setup(
         "dill>=0.3.9",
         "termcolor>=3.1.0",
         "requests",
-        "pypdf~=5.8.0",
+        "pypdf~=6.0.0",
         "python-docx",
         "pandas",
         "chromadb",
